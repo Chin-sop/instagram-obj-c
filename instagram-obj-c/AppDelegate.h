f8e9e2f9a3fe662fa27dbf5987f6aad40b907bf9
Chizy <chizy7@gmail.com>
2022-06-02 02:00:21 -0400

    Initial Commit

---
+++ instagram-obj-c/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  instagram-obj-c
+//
+//  Created by Chizaram Chibueze on 6/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ instagram-obj-c/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  instagram-obj-c
+//
+//  Created by Chizaram Chibueze on 6/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
