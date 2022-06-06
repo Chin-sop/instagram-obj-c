9d5fc942301fdf1b412e5ee684884985517a87b8
Chizy <chizy7@gmail.com>
2022-06-06 10:43:28 -0400

    Post query

--- instagram-obj-c/Models/PostCell.h
+++ instagram-obj-c/Models/PostCell.h
@@ -12,6 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PostCell : UITableViewCell
 
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+
+
 @property (strong, nonatomic) Post *post;
 
 @end
